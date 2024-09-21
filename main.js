121dac1f53bb8c591a6e8f58bb475d5035cb92e7
Nguyễn Hồng Sơn <snguyenhong8@MacBook-Pro-cua-Nguyen.local>
2024-09-22 00:20:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+/*
+var listUser = [
+    {
+        name: "Nguyen thi B",
+        age: 18
+    },
+    {
+        name: "Le Van A",
+        age: 18
+    },
+    {
+        name: "Do thi C",
+        age: 18
+    },
+];
+for (item of listUser){
+    console.log(item);
+}
+for (item of listUser) {
+    console.log(item.name);
+}
+    */
+// While: Luôn luôn kiểm tra điều kiện trước
+// while (condition){
+//              code 
+// 
+// var i = 0;
+// do {
+//     console.log(i);
+//     if (i>=2) {
+//         break;
+//     }
+//     i++
+// } while (i<=3);
+// for (var i = 0 ; i < 10; i++){
+//     if (i == 5)
+//     {
+//         continue; //call of the boucle
+//     }
+//     console.log(i);
+// }
+// Continue
+// Bỏ qua 1 bước lặp nào đó: Nghĩa là gặp lệnh continue thì tất cả những đoạn code nằm bên dưới sẽ
+// không được thực hiện mà nhảy qua vòng lặp mới luôn
+if (true){
+    let a = 10;
+    console.log(a);
+}
+console.log(a);
